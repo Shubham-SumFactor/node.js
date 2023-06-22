d0a23c0bb6fbf69f96ff277c637e00effc6967d8
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-23 02:25:35 +0530

    Connection to Database

---
+++ src/Database/ConnectDatabase.ts
@@ -0,0 +1,41 @@
+import mysql from 'mysql';
+
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "shubham123",
+    database: "nodetutorialdb"
+})
+
+
+export const connectNodeDatabase = () => {
+    return new Promise((resolve, reject) => {
+        try {
+
+            connection.connect((error) => {
+                if (error) return reject(error);
+                return resolve("Database connected Successfully")
+            })
+
+        } catch (error) {
+            console.log("🚀 ~ file: ConnectDatabase.ts:16 ~ return newPromise ~ error:", error)
+        }
+
+    })
+}
+
+// export const executeQuery = (sqlQuery: string) => {
+    // return new Promise((resolve, reject) => {
+        // try {
+            // connection.connect((error, response) => {
+                // console.log("🚀 ~ file: ConnectDatabase.ts:31 ~ connection.query ~ error:", error)
+                // if (error) return reject(error)
+// 
+                // return resolve("Database connected Successfully")
+            // })
+        // } catch (error) {
+            // console.log("🚀 ~ file: ConnectDatabase.ts:32 ~ return newPromise ~ error:", error)
+// 
+        // }
+    // })
+// }
