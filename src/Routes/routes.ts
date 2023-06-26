59c13db934abcc1bf9f1c17bd39fe8aa846d935a
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-26 16:42:34 +0530

    POST userLogin route created

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response, Router } from 'express';
 import { userRegistration } from '../Controller/userController';
+import { userlogin } from '../Controller/userController';
 
 const router = Router();
 
 router.post('/registerUser', async (req: Request, res: Response) => { res.status(200).send(await userRegistration(req, res)) })
 
-
+router.post('/userLogin', async (req: Request, res: Response) => { res.status(200).send(await userlogin(req, res)) })
 
 
 export default router;
