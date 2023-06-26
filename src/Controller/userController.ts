c63660a26dffe64dcf1a9afcd89f3e8822db638b
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-26 16:40:57 +0530

    userLogin implemented

--- src/Controller/userController.ts
+++ src/Controller/userController.ts
@@ -1,6 +1,8 @@
- import { executeQuery } from '../Database/ConnectDatabase'
-// 
-// 
+
+import { executeQuery } from '../Database/ConnectDatabase'
+
+import bcrypt from 'bcrypt';
+
  export const userRegistration = async (req: any, res: any) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -8,7 +10,7 @@
             let { f_name, l_name, email, password, token } = req.body
             console.log(req.body, "in body")
  //=========================== INSERT QUERY ===========================
-            const sqlQuery = `insert into user_table (f_name, l_name, email, password, token)  values ('${f_name}', '${l_name}', '${email}', '${password}', '${token}')`;
+            // const sqlQuery = `insert into user_table (f_name, l_name, email, password, token)  values ('${f_name}', '${l_name}', '${email}', '${password}', '${token}')`;
             // const sqlQuery = `insert into user_bankdetails (f_name, email, ifsc, account_number)values('Shubham', 'shubham.sumfactor@gmail.com', 'PUNB000', '123456789')`
 
 //=========================== Data-Retrieval QUERY ===========================
@@ -23,10 +25,24 @@
 //=========================== Join QUERY ===========================
             // const sqlQuery = `SELECT * FROM user_table as a join user_bankdetails as b  on a.email= b.email;`
 
+//====================================Validation for duplicacy============
+
+            const getRecord = `SELECT * from user_table where email='${email}'`
+            let resultset: any = await executeQuery(getRecord);
+
+            if (resultset.length > 0) return res.status(400).send({ message: "User already registered please login" });
+// =====================================================
+
+            // Encrypting the Password========================
+            const  salt = await bcrypt.genSalt();
+            const hashedPassword = await bcrypt.hash(password,salt);
+            //================================================================ 
+            
+            const sqlQuery = `insert into user_table (f_name,l_name,email,password,token)values('${f_name}', '${l_name}', '${email}', '${hashedPassword}', '${token}')`
             let response = await executeQuery(sqlQuery)
  
              console.log("🚀 ~ file: userController.ts:13 ~ returnnewPromise ~ response:", response)
-// 
+
              return resolve(response)
 
         } catch (error) {
@@ -36,12 +52,26 @@
     })
 }
 
-// export const userRegistration = (req:any, res:any) => {
-    // return new Promise((resolve, reject)=>{
-        // try{
-            // return resolve("Hello World")
-        // }catch (error){
-            // return reject(error)
-        // }
-    // }) 
-// }
+
+//=========================== USER LOGIN ===========================
+export const userlogin = async (req: any, res: any) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            let { email, password } = req.body
+
+            const getRecord = `select * from user_table where email='${email}'`
+
+            let resulset: any = await executeQuery(getRecord)
+
+            if (resulset.length == 0) return res.status(404).send({ message: "user Not Found Please Register and try again to login" })
+
+            const match: boolean = await bcrypt.compare(password, resulset[0].password)
+
+            if (match == false) return res.status(400).send("Entered Password is Incorrect")
+
+            return resolve(resulset)
+        }catch(error){
+            console.log("new Promise error", error)
+        }
+    })}
