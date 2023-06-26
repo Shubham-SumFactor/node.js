99ba65407c5f03a596a5a0df0b084d274048c0da
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-27 01:11:24 +0530

    route for the userlogin and getAllUser|verify token Added

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Router } from 'express';
 import { userRegistration } from '../Controller/userController';
-import { userlogin } from '../Controller/userController';
+import { userlogin, getAllUser} from '../Controller/userController';
+import { verifyToken } from '../util/service';
 
 const router = Router();
 
@@ -8,5 +9,6 @@ router.post('/registerUser', async (req: Request, res: Response) => { res.status
 
 router.post('/userLogin', async (req: Request, res: Response) => { res.status(200).send(await userlogin(req, res)) })
 
+router.get('/getAllUser', verifyToken, async (req: Request, res: Response) => { res.status(200).send(await getAllUser(req, res)) })
 
 export default router;
